15c3121aa6112ed58383bcde58eceeecd6cf7f6a
Osman Abdelnasir <esaminu1@icloud.com>
2020-05-18 05:11:49 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,37 +1,14 @@
-class Item {
-    public:
-        int index;
-        int value;
-        Item(int index, int value) {
-            this->index = index;
-            this->value = value;
-        }
-        bool operator==(const Item& t) const
-        {
-            return value == t.value;
-        }
-};
-
-class MyHashFunction { 
-public: 
-    size_t operator()(const Item& t) const
-    { 
-        return t.value; 
-    } 
-};
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_set<Item, MyHashFunction> itemSet; 
+        unordered_map<int,int> map;
+        
         for(int i = 0; i < nums.size(); i++) {
-            unordered_set<Item>::const_iterator it = itemSet.find(Item(-1,nums[i]));
-            if(it == itemSet.end()) {
-                itemSet.insert(Item(i, target - nums[i]));
-            } else {
-                return vector<int>{it->index,i};
+            if(map.find(nums[i]) != map.end()) {
+                return {map[nums[i]], i};
             }
+            map[target - nums[i]] = i;
         }
-        return vector<int>{0,0};
+        return {0,0};
     }
 };
