93a8d6beecdcb8e1f416addb1ba09bd9eaeb6a44
Osman Abdelnasir <esaminu1@icloud.com>
2020-08-03 02:44:53 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (javascript)

--- problems/longest_substring_without_repeating_characters/solution.js
+++ problems/longest_substring_without_repeating_characters/solution.js
@@ -2,20 +2,19 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    // if (s.length === 0) return 0
-    // if (s.length === 1) return 1
-    var lengths = [];
-    var letters = [];
-    for(i=0;i<s.length;i++){
-        if(letters.length == 0 || !letters.includes(s[i])){
-            letters.push(s[i])
-        } else if(letters.includes(s[i])){
-            lengths.push(letters.length)
-            letters = letters.indexOf(s[i]) === letters.length - 1 ? [s[i]] : letters.slice(letters.indexOf(s[i]) + 1, letters.length).concat([s[i]])
+var lengthOfLongestSubstring = function(str) {
+    let freq = {};
+    let currLongest = Number.MIN_VALUE;
+    let windowStart = 0, windowEnd = 0;
+  
+    while(windowEnd <= str.length) {
+        if(!freq[str[windowEnd]]) {
+          currLongest = Math.max(currLongest, windowEnd - windowStart);
+          freq[str[windowEnd++]] = 1;
+        } else {
+          currLongest = Math.max(currLongest, windowEnd - windowStart);
+          freq[str[windowStart++]]-=1;
         }
     }
-    var max  = Math.max(...lengths)
-    return  max > letters.length ? max : letters.length
-    
+    return currLongest;
 };
